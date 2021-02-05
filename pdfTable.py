678ea1cbedbae0d583747f8f04502a9e1b3adae3
Evan Rushton <evan.rushton@gmail.com>
2021-02-04 17:09:34 -0800

    pdfTable works for BoA

--- pdfTable.py
+++ pdfTable.py
@@ -4,35 +4,40 @@
 import tabula
 import sys
 import pandas as pd
-
+import re
 data = [] # list of three-value lists
 
 
 # Collect filename and pages from sys.argv
 inFile = sys.argv[1] 
-# outFile = sys.argv[2]
-# numPages = sys.argv[3]
+outFile = sys.argv[2]
+# type = sys.argv[3]
+
+# Bank of America hard coded statments
+def trimCols(table):
+  cols = len(table.columns)
+  if cols == 4: # keep cols 0,2
+    return(table.iloc[:, [True, False, True, False]])
+  elif cols == 5: # keep cols 0,3 
+    return(table.iloc[:, [True, False, False, True, False]])
+
+def bankOfAmerica():
+  data = pd.concat(list((trimCols(table) for table in tables)), ignore_index=True)
+  data.columns = ["Description", "Amount"]
+  # detect date ([0-1][1-9]/[0-3][0-9]\s/b)
+  # rest of text ([a-zA-Z0-9\s.\-]+$) 
+  # $ value (^\$[0-9]+\.[0-9][0-9])
+  tmp = pd.DataFrame() 
+  tmp  = data.iloc[:,0].str.extract(r'([0-1][1-9]/[0-3][0-9]\s)(?=[^0-1][^1-9][^/][^0-3][^0-9])(.*$)')
+  data['Date'] = tmp[0]
+  data['Description'] = tmp[1]
+  data = data[['Date', 'Amount', 'Description']]
+  return data
 
 # Generate csv from pdf in one table
 # tabula.convert_into(inFile, outFile, pages='all', guess=False)
 
 # Generate individual tables
-tables = tabula.read_pdf(inFile, pages='all', multiple_tables=True, guess=False)
-# Print to console
-#i = 0
+tables = tabula.read_pdf(inFile, pages='all', multiple_tables=True, guess=False) # type list
 
-# detect date ([0-1][1-9]/[0-3][0-9]\s/b)
-# rest of text ([a-zA-Z0-9\s.\-]+$) 
-# $ value (^\$[0-9]+\.[0-9][0-9])
-data = tables.str.extract(r'([0-1][1-9]/[0-3][0-9]\s/b)([a-zA-Z0-9\s.\-]+$)(^\$[0-9]+\.[0-9][0-9])')
-print(data)
-
-# Convert data list to df and save as csv
-# df = pd.DataFrame(data, columns = ['date', 'amount', 'description'])
-
-  #print(table)
-  #i +=1
-#print(type(tables))
-#print(type(tables[0]))
-
-
+bankOfAmerica().to_csv(outFile)
